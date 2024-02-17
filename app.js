d7d16aacff3110249de72bfe5e5ffbb622c03987
Alireza <alireza.hamami@gmail.com>
2024-02-17 12:44:35 -0400

    add node

--- app.js
+++ app.js
@@ -1,41 +1,9 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3000; // Set the port, default to 3000
 
-const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
-    const extname = path.extname(filePath);
-    let contentType = 'text/html';
+app.use(express.static(__dirname)); 
 
-    // Set content type based on file extension
-    switch (extname) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-    }
 
-    // Serve the file
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code == 'ENOENT') {
-                res.writeHead(404);
-                res.end('File not found');
-            } else {
-                res.writeHead(500);
-                res.end('Internal server error');
-            }
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
-});
-
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen('3000', () => console.log('we started listening 3000'))
