ff0a05fbbe6cf29af9a7233a34de53d10e41d61e
Alireza <alireza.hamami@gmail.com>
2024-02-17 17:43:51 -0400

    final

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000; // Set the port, default to 3000
+const PORT = process.env.PORT || 5001; // Set the port, default to 3000
 
 app.use(express.static(__dirname)); 
 
 
-app.listen('3000', () => console.log('we started listening 3000'))
+app.listen('5001', () => console.log('we started listening 5001'))
